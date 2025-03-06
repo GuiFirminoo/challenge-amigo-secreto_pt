3955ad6c7c1a05a2b6d14198914d3dff70792246
GuiFirminoo <guiwill942@gmail.com>
2025-03-05 21:47:24 -0300

    Construindo o esqueleto do desafio

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigos() {
+    let amigoEscolhido = document.querySelector("input").value;
+    if (amigoEscolhido === "") {
+        alert("Digite um nome para adicionar ao sorteio");
+    } else {
+        amigos.push(amigoEscolhido);
+        limparCampo();
+        mostrarAmigosNaLista();
+    }
+}
+
+function limparCampo() {
+    amigoEscolhido = document.querySelector("input");
+    amigoEscolhido.value = "";
+}
+
+function sortearAmigos() {
+    paragrafoAmigos = document.getElementById("listaAmigos");
+    paragrafoAmigos.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+       let item = document.createElement("li");
+       item.textContent = amigos[i];
+       paragrafoAmigos.appendChild(item);
+    }
+}
