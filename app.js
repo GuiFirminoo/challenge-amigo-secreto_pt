285530ad7a4b50720a070842684992d738ae480f
GuiFirminoo <guiwill942@gmail.com>
2025-03-06 21:30:15 -0300

    Criando a função de sortear amigos e validando se há nomes existentes na lista para que o usuário clique em "Sortear amigos"

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ const inputAmigo = document.getElementById("amigo");
 const ulListaAmigos = document.getElementById("listaAmigos");
 const ulResultado = document.getElementbyID("resultado");
 
-function adicionarAmigo() {
+//Função para ADICIONAR amigos
+function adicionarAmigo() { //Validar que o campo de entrada não esteja vazio
     if (!inputAmigo.value) {
         alert("Necessário adicionar pelo menos um nome na lista!");
         return;
     } 
-
+// Após validação, realizar a adição do amigo informado
     listaAmigos.push(inputAmigo.value);
     ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
 
-    // Limpeza do campo de entrada
+// Limpeza do campo de entrada após inserção de amigo na lista
     inputAmigo.value = "";
+}
+//Fim da função para ADICIONAR amigos
+//Inicio da função para SORTEAR os amigos
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+    alert("Não há amigos para o sorteio! Adicione pelo menos um nome para o sorteio.");
+    return
+   } 
 }
